43c826e795d2a3e53f4b79028dd77b89bd539453
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-04-26 15:51:21 +0700

    Generic Struct

---
+++ generic_struct_test.go
@@ -0,0 +1,40 @@
+package go_generics
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type Data[T any] struct {
+	First  T
+	Second T
+}
+
+func (d *Data[_]) SayHello(name string) string {
+	return "Hello " + name
+}
+
+func (d *Data[T]) ChangeFirst(first T) T {
+	d.First = first
+	return d.First
+}
+
+func TestData(t *testing.T) {
+	data := Data[string]{
+		First:  "Taufiq",
+		Second: "Kurniawan",
+	}
+
+	fmt.Println(data)
+}
+
+func TestGenericMethod(t *testing.T) {
+	data := Data[string]{
+		First:  "Taufiq",
+		Second: "Kurniawan",
+	}
+
+	assert.Equal(t, "Budi", data.ChangeFirst("Budi"))
+	assert.Equal(t, "Hello Kurniawan", data.SayHello("Kurniawan"))
+}
