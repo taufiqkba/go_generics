04cea39c5e443599e6e92179c5e10c996b923774
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-04-26 11:22:13 +0700

    Type Approximation

--- type_sets_test.go
+++ type_sets_test.go
@@ -5,8 +5,11 @@ import (
 	"testing"
 )
 
+type Age int
+
+// ~ = using for type approximation to make type Age include to Number constraint
 type Number interface {
-	int | int8 | int16 | int32 | int64 | float32 | float64
+	~int | int8 | int16 | int32 | int64 | float32 | float64
 }
 
 func Min[T Number](first, second T) T {
@@ -21,4 +24,5 @@ func TestMin(t *testing.T) {
 	assert.Equal(t, 100, Min[int](100, 200))
 	assert.Equal(t, int64(100), Min[int64](int64(100), int64(200)))
 	assert.Equal(t, float64(100), Min[float64](float64(100), float64(200)))
+	assert.Equal(t, Age(100), Min[Age](Age(100), Age(200)))
 }
